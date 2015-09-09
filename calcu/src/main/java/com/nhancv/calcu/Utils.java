d7197b5ab112f806cb9aae86d274d93c8a9af15c
cvnhan <caovannhan2002@gmail.com>
2015-09-09 22:27:30 +0700

    test

--- calcu/src/main/java/com/nhancv/calcu/Utils.java
+++ calcu/src/main/java/com/nhancv/calcu/Utils.java
@@ -4,7 +4,10 @@ package com.nhancv.calcu;
  * Created by NhanCao on 09-Sep-15.
  */
 public class Utils {
-    public static int cal(int a, int b){
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    public static int sub(int a, int b){
         return a+b;
     }
 }
